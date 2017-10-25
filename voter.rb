95c9cc708e34a5087b3c692ac330fb5520aa95fc
James Hartman <jameshartman@Jamess-MacBook-Pro.local>
2017-10-25 09:37:12 -0400

    Completed voter data entry in array of hashes

---
+++ voter.rb
@@ -0,0 +1,83 @@
+# input demographics into a nested data structure
+demographics = [
+  {
+             "name" => "John Smith",
+              "age" => 25,
+           "income" => 50_000,
+   "household_size" => 1,
+           "gender" => "male",
+        "education" => "college"
+  },
+  {
+             "name" => "Jane Davies",
+              "age" => 30,
+           "income" => 60_000,
+   "household_size" => 3,
+           "gender" => "female",
+        "education" => "high school"
+  },
+  {
+             "name" => "Sam Farelly",
+              "age" => 32,
+           "income" => 80_000,
+   "household_size" => 2,
+           "gender" => "unspecified",
+        "education" => "college"
+  },
+  {
+             "name" => "Joan Favreau",
+              "age" => 35,
+           "income" => 65_000,
+   "household_size" => 4,
+           "gender" => "female",
+        "education" => "college"
+  },
+  {
+             "name" => "Sam McNulty",
+              "age" => 38,
+           "income" => 63_000,
+   "household_size" => 3,
+           "gender" => "male",
+        "education" => "college"
+  },
+  {
+             "name" => "Mark Minahan",
+              "age" => 48,
+           "income" => 78_000,
+   "household_size" => 5,
+           "gender" => "male",
+        "education" => "high school"
+  },
+  {
+             "name" => "Susan Umani",
+              "age" => 45,
+           "income" => 75_000,
+   "household_size" => 2,
+           "gender" => "female",
+        "education" => "college"
+  },
+  {
+             "name" => "Bill Perault",
+              "age" => 24,
+           "income" => 45_000,
+   "household_size" => 1,
+           "gender" => "male",
+        "education" => "did not complete high school"
+  },
+  {
+             "name" => "Doug Stamper",
+              "age" => 45,
+           "income" => 75_000,
+   "household_size" => 1,
+           "gender" => "male",
+        "education" => "college"
+  },
+  {
+             "name" => "Francis Underwood",
+              "age" => 52,
+           "income" => 100_000,
+   "household_size" => 2,
+           "gender" => "male",
+        "education" => "college"
+  }
+]
